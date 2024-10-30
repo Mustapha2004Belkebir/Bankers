1967e31b9fdc269dc51f89899b16e8bc672d382e
neon-x-hub <abdomem7@gmail.com>
2024-10-30 11:56:05 +0000

    Add pagination/date column + Update search functoinality.

--- Lab 3/Homework/DB/DB_Client.py
+++ Lab 3/Homework/DB/DB_Client.py
@@ -19,33 +19,83 @@ class ExpensesDatabaseClient:
             CREATE TABLE IF NOT EXISTS expenses (
                 exp_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 expense TEXT NOT NULL,
-                price REAL NOT NULL
+                price REAL NOT NULL,
+                date DATE NOT NULL
             );
         ''')
         self.cursor.execute('CREATE INDEX IF NOT EXISTS idx_expense ON expenses(expense);')
         self.cursor.execute('CREATE INDEX IF NOT EXISTS idx_price ON expenses(price);')
         self.connection.commit()
 
-    def create_expense(self, expense: str, price: float) -> None:
-        """Add a new expense to the database."""
-        self.cursor.execute("INSERT INTO expenses (expense, price) VALUES (?, ?);", (expense, price))
+    def create_expense(self, expense: str, price: float, date: str) -> None:
+        """Add a new expense to the database with a provided date."""
+        self.cursor.execute(
+            "INSERT INTO expenses (expense, price, date) VALUES (?, ?, ?);",
+            (expense, price, date)
+        )
         self.connection.commit()
 
-    def read_expenses(self) -> List[Tuple[int, str, float]]:
-        """Retrieve all expenses from the database."""
-        self.cursor.execute("SELECT * FROM expenses;")
+    def read_all_expenses(self, limit: int, offset: int = 0) -> List[Tuple[int, str, float, str]]:
+        """Retrieve expenses from the database with pagination."""
+        self.cursor.execute(
+            "SELECT * FROM expenses LIMIT ? OFFSET ?;", 
+            (limit, offset)
+        )
         return self.cursor.fetchall()
 
-    def update_expense(self, exp_id: int, expense: Union[str, None] = None, price: Union[float, None] = None) -> None:
+
+    def update_expense(self, exp_id: int, expense: Union[str, None] = None, price: Union[float, None] = None, date: Union[str, None] = None) -> None:
         """Update an expense's details in the database."""
-        if expense is not None and price is not None:
+        # Prepare the SQL query based on which parameters are provided
+        if expense is not None and price is not None and date is not None:
+            self.cursor.execute("UPDATE expenses SET expense = ?, price = ?, date = ? WHERE exp_id = ?;", (expense, price, date, exp_id))
+        elif expense is not None and price is not None:
             self.cursor.execute("UPDATE expenses SET expense = ?, price = ? WHERE exp_id = ?;", (expense, price, exp_id))
+        elif expense is not None and date is not None:
+            self.cursor.execute("UPDATE expenses SET expense = ?, date = ? WHERE exp_id = ?;", (expense, date, exp_id))
+        elif price is not None and date is not None:
+            self.cursor.execute("UPDATE expenses SET price = ?, date = ? WHERE exp_id = ?;", (price, date, exp_id))
         elif expense is not None:
             self.cursor.execute("UPDATE expenses SET expense = ? WHERE exp_id = ?;", (expense, exp_id))
         elif price is not None:
             self.cursor.execute("UPDATE expenses SET price = ? WHERE exp_id = ?;", (price, exp_id))
+        elif date is not None:
+            self.cursor.execute("UPDATE expenses SET date = ? WHERE exp_id = ?;", (date, exp_id))
+            
         self.connection.commit()
 
+    def search_expenses(self, expense: Union[str, None] = None, price: Union[float, None] = None, date: Union[str, None] = None, limit: int = 10, offset: int = 0) -> List[Tuple[int, str, float, str]]:
+        """Search expenses by filtering on expense, price, and/or date with pagination."""
+        # Base query
+        query = "SELECT * FROM expenses WHERE"
+        filters = []
+        values = []
+
+        # Add filters based on provided parameters
+        if expense is not None:
+            filters.append("expense LIKE ?")
+            values.append(f"%{expense}%")  # Using LIKE for partial matches
+        if price is not None:
+            filters.append("price = ?")
+            values.append(price)
+        if date is not None:
+            filters.append("date = ?")
+            values.append(date)
+
+        # If no filters are provided, use a base query with pagination
+        if not filters:
+            query = "SELECT * FROM expenses"
+        else:
+            # Join filters with AND
+            query += " AND ".join(filters)
+
+        # Add pagination
+        query += " LIMIT ? OFFSET ?"
+        values.extend([limit, offset])
+
+        # Execute the query
+        self.cursor.execute(query, values)
+        return self.cursor.fetchall()
     def delete_expense(self, exp_id: int) -> None:
         """Delete an expense from the database by ID."""
         self.cursor.execute("DELETE FROM expenses WHERE exp_id = ?;", (exp_id,))
